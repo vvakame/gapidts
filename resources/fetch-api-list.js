8e0eebce994499ab262b1a5e872ee9cca9d2582a
vvakame <vvakame+dev@gmail.com>
2014-08-29 01:47:01 +0900

    update dependencies

--- resources/fetch-api-list.js
+++ resources/fetch-api-list.js
@@ -1,23 +1,16 @@
 var googleapis = require('googleapis');
 
-googleapis
-	.discover('discovery', 'v1')
-	.execute(function (err, client) {
-		if (err) {
-			console.error('Problem during the client discovery.', err);
-			return;
-		}
-		client.discovery.apis.list().execute(function (err, response) {
-			if (err) {
-				console.error('Problem during the call discovery.apis.list.', err);
-				return;
-			}
-			var results = response.items.map(function (item) {
-				return {
-					name: item.name,
-					version: item.version
-				};
-			});
-			console.log(JSON.stringify(results, null, 2));
-		});
+var discovery = googleapis.discovery('v1');
+discovery.apis.list(function (err, response) {
+	if (err) {
+		console.error('Problem during the client discovery.', err);
+		return;
+	}
+	var results = response.items.map(function (item) {
+		return {
+			name: item.name,
+			version: item.version
+		};
 	});
+	console.log(JSON.stringify(results, null, 2));
+});
