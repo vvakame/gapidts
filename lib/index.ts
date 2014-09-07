9daf32d182aa336c85d2b8f7dadbb1898f0e2591
vvakame <vvakame+dev@gmail.com>
2014-09-07 20:07:54 +0900

    source-map-support to be optional

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,11 @@
 import _model = require("./model");
 import _emitter = require("./emitter/index");
 
-require("source-map-support").install();
+try {
+	// optional
+	require("source-map-support").install();
+} catch (e) {
+}
 
 function gapidts(json:string):_emitter.IResult {
 	var root:_model.IRestDescription = JSON.parse(json);
